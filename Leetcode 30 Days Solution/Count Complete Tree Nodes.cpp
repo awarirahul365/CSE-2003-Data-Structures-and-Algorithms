5027a372007205c6232df92abecc2a98c416cf6b
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2020-07-31 00:30:26 +0530

    Create Count Complete Tree Nodes.cpp

---
+++ Leetcode 30 Days Solution/Count Complete Tree Nodes.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        else
+        {
+            queue<TreeNode*>q;
+            q.push(root);
+            int count=0;
+            while(!q.empty())
+            {
+                for(int i=0;i<q.size();i++)
+                {
+                    TreeNode *curr=q.front();
+                    q.pop();
+                    count++;
+                    if(curr->left!=NULL)
+                    {
+                        q.push(curr->left);
+                    }
+                    if(curr->right!=NULL)
+                    {
+                        q.push(curr->right);
+                    }
+                }
+            }
+            return count;
+        }
+    }
+};
