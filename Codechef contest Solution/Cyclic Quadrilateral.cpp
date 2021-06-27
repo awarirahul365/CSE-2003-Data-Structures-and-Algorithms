50fb8997f7d7d7e6bea897863d845f26be9359db
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2021-06-27 17:44:36 +0530

    Create Cyclic Quadrilateral.cpp

---
+++ Codechef contest Solution/Cyclic Quadrilateral.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void func(int a,int b,int c,int d)
+{
+    if(a+c==180 && b+d==180)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+int main() {
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        func(a,b,c,d);
+    }
+}
