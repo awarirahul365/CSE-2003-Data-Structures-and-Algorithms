b3b10f83bd4d649dab4091ebbb1391d74f0a53d9
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2021-06-28 23:38:50 +0530

    Update topic2.cpp

--- OOPS Concepts/topic2.cpp
+++ OOPS Concepts/topic2.cpp
@@ -1,1 +1,16 @@
-#
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class A{
+	public:
+		int age;
+	private:
+		int salary;
+};
+int main()
+{
+	A obj;
+	obj.age=21;
+	obj.salary=300;//not allowed as private cannot be accessed
+	cout<<obj.age<<" "<<obj.salary<<endl;
+}
