66310e0d4995e354c31d9f490e7611a56955c274
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2021-07-27 12:10:11 +0530

    Create Seat Reservation Manager.cpp

---
+++ Leetcode 30 Days Solution/Seat Reservation Manager.cpp
@@ -0,0 +1,34 @@
+class SeatManager {
+public:
+    priority_queue<int,vector<int>,greater<int>>pq;
+    SeatManager(int n) {
+        
+        for(int i=1;i<=n;i++)
+        {
+            pq.push(i);
+        }
+    }
+    
+    int reserve() {
+        
+        int num;
+        if(!pq.empty())
+        {
+            num=pq.top();
+            pq.pop();
+        }
+        return num;
+    }
+    
+    void unreserve(int seatNumber) {
+        
+        pq.push(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
