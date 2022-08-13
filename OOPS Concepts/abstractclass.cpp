3c3115b858370874f7249186c50040aad7e62d89
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2022-08-13 10:19:29 +0530

    Update abstractclass.cpp

--- OOPS Concepts/abstractclass.cpp
+++ OOPS Concepts/abstractclass.cpp
@@ -36,3 +36,19 @@ int main()
 	r.getArea();
 	cout<<r.getArea()<<" "<<t.getArea();
 }
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code 
+    int arr[5]={1,2,3,4,5};
+    int *q=&arr[0];
+    int (*z)[5]=&arr;
+    char *t="abcdef";
+    
+    std::cout<<&t[0];
+    
+
+    return 0;
+}
